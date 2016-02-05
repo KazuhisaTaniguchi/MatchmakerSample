f5a70a43ca7a73809593035604c68ce0aadafab3
KazuhisaTaniguchi <jjzoow96@gmail.com>
2016-02-05 10:42:47 +0900

    some changed

--- src/matches/models.py
+++ src/matches/models.py
@@ -1,5 +1,10 @@
-from django.db import models
+import datetime
+
+
 from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
 from .utils import get_match
 
 
@@ -13,26 +18,33 @@ class MatchManager(models.Manager):
             obj_2 = self.get(user_a=user_b, user_b=user_a)
         except:
             obj_2 = None
-
         if obj and not obj_2:
+            obj.check_update()
             return obj, False
         elif not obj and obj_2:
+            obj_2.check_update()
             return obj_2, False
         else:
             new_instance = self.create(user_a=user_a, user_b=user_b)
-            # add match %
-            match_decimal, questions_answered = get_match(user_a=user_a, user_b=user_b)
-            new_instance.match_decimal = match_decimal
-            new_instance.questions_answered = questions_answered
-            new_instance.save()
+            new_instance.do_match()
             return new_instance, True
+
+    def update_all(self):
+        queryset = self.all()
+        now = timezone.now()
+        offset = now - datetime.timedelta(hours=12)
+        offset2 = now - datetime.timedelta(hours=36)
+        queryset.filter(updated__gt=offset2).filter(updated__lte=offset)
+        if queryset.count > 0:
+            for i in queryset:
+                i.check_update()
 
 
 class Match(models.Model):
     user_a = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name="match_user_a")
+        settings.AUTH_USER_MODEL, related_name='match_user_a')
     user_b = models.ForeignKey(
-        settings.AUTH_USER_MODEL, related_name="match_user_b")
+        settings.AUTH_USER_MODEL, related_name='match_user_b')
     match_decimal = models.DecimalField(
         decimal_places=8, max_digits=16, default=0.00)
     questions_answered = models.IntegerField(default=0)
@@ -44,6 +56,9 @@ class Match(models.Model):
 
     objects = MatchManager()
 
+    # good match?
+    # percentage value?
+
     def do_match(self):
         user_a = self.user_a
         user_b = self.user_b
@@ -52,4 +67,11 @@ class Match(models.Model):
         self.questions_answered = questions_answered
         self.save()
 
-
+        def check_update(self):
+            now = timezone.now()
+            # 12 seconds ago
+            offset = now - datetime.timedelta(hours=12)
+            if self.updated <= offset:
+                self.do_match()
+            else:
+                print("already updated")
